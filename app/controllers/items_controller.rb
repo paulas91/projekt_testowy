8a3f89b5b7e4d51d0d8395fcdb6799969dfa1e5a
paulas91 <paulinaskopp@gmail.com>
2022-12-16 12:36:00 +0100

    Add custom methods.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,6 +57,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def active
+    @items = Item.active
+  end
+
+  def inactive
+    @items = Item.inactive
+  end
+
+  def borrowed
+    @items = Item.borrowed
+  end
+
+  def on_place
+    @items = Item.on_place
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :items
   root 'items#index'
+  resources :items do
+    collection do
+      get :active
+      get :inactive
+      get :borrowed
+      get :on_place
+    end
+  end
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
