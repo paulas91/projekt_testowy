7c0dc4600b5006fbaaba625ab43b6928e04b8c0d
paulas91 <paulinaskopp@gmail.com>
2022-12-20 11:25:45 +0100

    Rspec validations tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,11 @@ describe Item, type: :model do
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_length_of(:name).is_at_least(3) }
+    it { is_expected.to validate_length_of(:name).is_at_most(25) }
+    it { is_expected.to define_enum_for(:item_type).with_values(Item.item_types) }
+  end
 end
