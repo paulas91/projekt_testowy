36617c7214050e66a684a064c5e096f8d77a4994
paulas91 <paulinaskopp@gmail.com>
2023-01-19 14:07:35 +0100

    bootstrap modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   devise_for :users
   root 'items#index'
   resources :items do
@@ -11,5 +12,8 @@ Rails.application.routes.draw do
       get :on_place
     end
   end
+
   resources :users
+  resources :friends, only: %i[index show]
+
 end
