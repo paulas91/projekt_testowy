4500b64e57ab3ac5e1865b62b5e49bc9cc969370
paulas91 <paulinaskopp@gmail.com>
2023-01-24 14:47:15 +0100

    state machine-status calls

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,6 +5,7 @@
 # Table name: invitations
 #
 #  id            :bigint           not null, primary key
+#  state         :string
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  invited_by_id :bigint           not null
@@ -16,6 +17,21 @@
 #  index_invitations_on_invited_id     (invited_id)
 #
 class Invitation < ApplicationRecord
+  include AASM
+
+  aasm column: 'state' do
+    state :pending, initial: true
+    state :accepted, :rejected
+
+    event :accept do
+      transitions from: :pending, to: :accepted
+    end
+
+    event :reject do
+      transitions form: :pending, to: :rejected
+    end
+  end
+
   belongs_to :invited_user, class_name: 'User', foreign_key: :invited_id, optional: true
   belongs_to :inviting_user, class_name: 'User', foreign_key: :invited_by_id
 end
