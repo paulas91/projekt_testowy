27143f075c28361c2d5f0935a983a93a58c09ce6
paulas91 <paulinaskopp@gmail.com>
2023-01-26 18:48:25 +0100

    real-time notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -17,5 +17,11 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Notification < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :user
+
+  after_create_commit do
+    broadcast_append_to [user, :notifications], target: 'notifications_list', partial: 'notifications/notification', locals: { notification: self }
+    broadcast_replace_to [user, :notifications], target: dom_id(user, :notifications_badge), partial: 'notifications/notifications_badge', locals: { user: user}
+  end
 end
